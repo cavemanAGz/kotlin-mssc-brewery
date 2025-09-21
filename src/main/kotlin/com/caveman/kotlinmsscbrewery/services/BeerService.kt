25068ad0f0ff938e6e05ab8befe093ad55e32cde
Alex Wood <alexanderclaywood@msn.com>
2025-09-20 17:08:33 -0700

    Section 4: Lesson 35 - Complete

--- src/main/kotlin/com/caveman/kotlinmsscbrewery/services/BeerService.kt
+++ src/main/kotlin/com/caveman/kotlinmsscbrewery/services/BeerService.kt
@@ -6,6 +6,6 @@ import java.util.UUID
 interface BeerService {
     fun getBeerById(beerId: UUID): BeerDto
     fun saveNewBeer(newBeer: BeerDto): BeerDto
-    fun updateBeer(beerId: java.util.UUID, updatedBeer: com.caveman.kotlinmsscbrewery.web.model.BeerDto)
+    fun updateBeer(beerId: UUID, updatedBeer: com.caveman.kotlinmsscbrewery.web.model.BeerDto)
     fun deleteBeer(beerId: UUID)
 }
